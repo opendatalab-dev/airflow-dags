b594729de60d5d275ef3b43913758fa52af994cb
Hammad KHAN <raohammad@gmail.com>
2022-11-09 04:36:34 +0100

    k8

--- test/dags/hello.py
+++ test/dags/hello.py
@@ -10,6 +10,9 @@ dag = DAG('hello_world', description='Hello World DAG',
           schedule_interval='0 12 * * *',
           start_date=datetime(2017, 3, 20), catchup=False)
 
-hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
+hello_operator = PythonOperator(
+    task_id='hello_task', 
+python_callable=print_hello, 
+dag=dag)
 
 hello_operator
